28707d37b1ce16e234c964e004eab6685144f097
Mike <mike999999@gmail.com>
2019-09-02 13:52:08 -0400

    Lucie completed French translation

--- languages/lang.fr.php
+++ languages/lang.fr.php
@@ -19,9 +19,324 @@
 
 /*
 ------------------
-Language: English
+Language: French
 ------------------
 */
 
 $langArr = array();
+$langArr['lang'] = 'fr';
 
+$langArr['PAGE_TITLE'] = 'Le projet Javaria';
+$langArr['APP_NAME'] = 'Le projet Javaria';
+
+// Common
+$langArr['CANCEL'] = 'Annuler';
+$langArr['CLOSE'] = 'Fermer';
+$langArr['UPDATE']='Mettre à jour';
+$langArr['DELETE']='Supprimer ';
+$langArr['SAVE']='Sauvegarder';
+$langArr['OK']='OK';
+$langArr['ALL']='Tout';
+$langArr['PLUS'] = '+';
+
+// Dashboard nav links
+$langArr['NAV-DASHBOARD_MENU'] = 'Tableau de bord';
+$langArr['NAV-DASHBOARD_ITEM_ADD'] = 'Ajouter Tableau de bord';
+
+// Blocks nav links
+$langArr['NAV-BLOCKS_MENU'] = 'Ajouter';
+$langArr['NAV-BLOCKS_ITEM_CHART'] = 'Bloc graphique';
+$langArr['NAV-BLOCKS_ITEM_HTML'] = 'Bloc HTML';
+
+// Admin nav links
+$langArr['NAV-ADMIN_MENU'] = 'Administrateur';
+$langArr['NAV-ADMIN_ITEM_USERS'] = 'Usagers';
+$langArr['NAV-ADMIN_ITEM_DASHBOARDS'] = 'Gérer tableaux de bords';
+$langArr['NAV-ADMIN_ITEM_THEMES'] = 'Thèmes';
+$langArr['NAV-ADMIN_ITEM_COLOURS'] = 'Gérer couleurs';
+$langArr['NAV-ADMIN_ITEM_GROUPS'] = 'Groupes';
+$langArr['NAV-ADMIN_ITEM_ROLES'] = 'Rôles';
+$langArr['NAV-ADMIN_ITEM_DATACONECTIONS'] = 'Connexions de données';
+$langArr['NAV-ADMIN_ITEM_LOGINAS'] = 'Se connecter en tant que...';
+$langArr['NAV-ADMIN_ITEM_PREFERENCES'] = 'Préférences';
+
+// Language nav links
+$langArr['NAV-LANGUAGE_MENU'] = 'Langues';
+$langArr['NAV-LANGUAGE_ITEM_EN'] = 'Anglais';
+$langArr['NAV-LANGUAGE_ITEM_FR'] = 'Français';
+
+// User nav links
+$langArr['NAV-USER_ITEM_PROFILE'] = 'Profile';
+$langArr['NAV-USER_ITEM_SIGNOUT'] = ' Quitter';
+
+// Modal Add dashboard
+$langArr['MODAL-ADD_DASHBOARD_TITLE'] = 'Tableau de bord';
+$langArr['MODAL-ADD_DASHBOARD_TXT_DASHBOARDNAME'] = 'Nom';
+$langArr['MODAL-ADD_DASHBOARD_SELECT_BACKGROUNDCOLOR'] = 'Couleur de fond';
+
+// Modal Add chart
+$langArr['MODAL-ADD_CHART_TITLE_ADD'] = 'Ajouter graphique';
+$langArr['MODAL-ADD_CHART_TITLE_EDIT'] = 'Mettre à jour graphique';
+$langArr['MODAL-ADD_CHART_TXT_NAME'] = 'Nom';
+
+$langArr['MODAL-ADD_CHART_SELECT_TYPE'] = 'Type de graphique';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_PIE'] = 'Graphique en secteurs';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_BAR'] = 'Barre';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_HBAR'] = 'Barre horizontale';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_POLAR'] = 'Polaire';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_LINE'] = 'Ligne';
+$langArr['MODAL-ADD_CHART_SELECT_TYPE_OPTION_DOUGHNUT'] = 'Graphique en anneau';
+
+$langArr['MODAL-ADD_CHART_TXT_XAXIS'] = 'Étiquette de l’axe X';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXIS'] = 'Montrer';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXISGRID'] = 'Grille';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXISTICKS'] = 'Ticks';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXISSTACKED'] = 'Empilé';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXISLINE'] = 'Ligne';
+$langArr['MODAL-ADD_CHART_CHK_SHOWXAXISLABEL'] = 'Étiquette';
+
+$langArr['MODAL-ADD_CHART_ELEMENT_YAXIS'] = 'Étiquette de l’axe Y';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXIS'] = 'Montrer';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXISGRID'] = 'Grille';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXISTICKS'] = 'Ticks';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXISSTACKED'] = 'Empilé';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXISLINE'] = 'Ligne';
+$langArr['MODAL-ADD_CHART_CHK_SHOWYAXISLABEL'] = 'Étiquette';
+
+$langArr['MODAL-ADD_CHART_CHK_SHOWLEGEND'] = 'Légende';
+
+$langArr['MODAL-ADD_CHART_SELECT_DATASET'] = 'Ensembles de données';
+$langArr['MODAL-ADD_CHART_BTN_ADD_DATASET'] = 'Ajouter';
+$langArr['MODAL-ADD_CHART_BTN_DELETE_DATASET'] = 'Supprimer';
+$langArr['MODAL-ADD_CHART_BTN_EDIT_DATASET'] = 'Modifier';
+
+$langArr['MODAL-ADD_CHART_BTN_USE_DATACONNECTION'] = 'Actualiser';
+
+$langArr['MODAL-ADD_CHART_BTN_ADD_CHART'] = 'Ajouter un graphique';
+$langArr['MODAL-ADD_CHART_BTN_EDIT_CHART'] = 'Modifier le graphique';
+
+$langArr['MODAL-SINGLE_DELETE_ELEMENT']= 'Voulez-vous supprimer cet élément?';
+
+// Modal Datasets
+$langArr['MODAL-ADD_DATASET_TITLE'] = 'Ensembles de données';
+$langArr['MODAL-ADD_DATASET_TXT_NAME'] = 'Nom de l’ensembles de données';
+$langArr['MODAL-ADD_DATASET_SELECT_DATACONNECTION'] = 'Connexion de données';
+$langArr['MODAL-ADD_DATASET_SELECT_TABLE'] = 'Sélectionner la table';
+$langArr['MODAL-ADD_DATASET_SELECT_THEME'] = 'Thème';
+$langArr['MODAL-ADD_DATASET_SELECT_LABEL'] = 'Colonne d’étiquette';
+$langArr['MODAL-ADD_DATASET_SELECT_VALUE'] = 'Colonne Valeur';
+
+$langArr['MODAL-ADD_DATASET_SELECT_AGGREGATION'] = 'Type d’agrégation';
+$langArr['MODAL-ADD_DATASET_SELECT_AGGREGATION_OPTION_SUM'] = 'SOMME';
+$langArr['MODAL-ADD_DATASET_SELECT_AGGREGATION_OPTION_AVG'] = 'MOYENNE';
+$langArr['MODAL-ADD_DATASET_SELECT_AGGREGATION_OPTION_COUNT'] = 'NOMBRE';
+$langArr['MODAL-ADD_DATASET_SELECT_AGGREGATION_OPTION_NONE'] = 'AUCUN';
+
+// Modal add html
+$langArr['MODAL-ADD_HTML_TITLE_ADD'] = 'Bloc HTML';
+$langArr['MODAL-ADD_HTML_TXT_NAME'] = 'Nom';
+$langArr['MODAL-ADD_HTML_TXT_CODE'] = 'Code HTML';
+
+// Modal user management
+$langArr['MODAL-USERS_MANAGE_TITLE'] = 'Usagers';
+$langArr['MODAL-USERS_MANAGE_SELECT_GROUPS'] = 'Groupes';
+$langArr['MODAL-USERS_MANAGE_LBL_USERNAME'] = 'Nom d’utilisateur';
+$langArr['MODAL-USERS_MANAGE_LBL_FULLNAME'] = 'Nom';
+$langArr['MODAL-USERS_MANAGE_LBL_EMAIL'] = 'Courriel';
+
+// Modal user add/edit
+$langArr['MODAL-USERS_ADDEDIT_TITLE_ADD'] = 'Ajouter usager';
+$langArr['MODAL-USERS_ADDEDIT_TITLE_EDIT'] = 'Modifier usager';
+$langArr['MODAL-USERS_ADDEDIT_TITLE_SELF'] = 'Profile';
+$langArr['MODAL-USERS_ADDEDIT_TXT_USERNAME'] = 'Nom d’utilisateur';
+$langArr['MODAL-USERS_ADDEDIT_TXT_FIRSTNAME'] = 'Prénom';
+$langArr['MODAL-USERS_ADDEDIT_TXT_LASTNAME']='Nom';
+$langArr['MODAL-USERS_ADDEDIT_TXT_PASSWORD']='Mot de passe';
+$langArr['MODAL-USERS_ADDEDIT_TXT_PHONENUMBER']='Numéro de téléphone';
+$langArr['MODAL-USERS_ADDEDIT_TXT_EMAIL']='Courriel';
+$langArr['MODAL-USERS_ADDEDIT_TXT_NOTES']='Notes';
+
+$langArr['MODAL-USERS_DELETE_TITLE'] = 'Supprimer usager';
+$langArr['MODAL-USERS_DELETE_TEXT'] = 'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal dashboard management
+$langArr['MODAL-DASHBOARDS_MANAGE_TITLE'] = 'Gérer tableaux de bords';
+$langArr['MODAL-DASHBOARDS_MANAGE_LBL_DASHBOARDS'] = 'Tableau de bord';
+
+$langArr['MODAL-DASHBOARDS_DELETE_TITLE'] = 'Supprimer tableau de bord';
+$langArr['MODAL-DASHBOARDS_DELETE_TEXT'] = 'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal dashboard assign
+$langArr['MODAL-DASHBOARDS_MEMBERS_TITLE_ADMINS'] = 'Attribuer admins du tableau de bord';
+$langArr['MODAL-DASHBOARDS_MEMBERS_TITLE_USERS'] = 'Attribuer usagers du tableau de bord';
+$langArr['MODAL-DASHBOARDS_MEMBERS_TXT_NAME'] = 'Nom';
+
+$langArr['MODAL-DASHBOARDS_MEMBERS_LBL_USERS'] = 'Usagers';
+$langArr['MODAL-DASHBOARDS_MEMBERS_LBL_SELECTEDUSERS'] = 'Usagers sélectionnés';
+
+// Modal theme management
+$langArr['MODAL-THEMES_MANAGE_TITLE'] = 'Gestion de thème';
+$langArr['MODAL-THEMES_MANAGE_LBL_THEME'] = 'Thème';
+$langArr['MODAL-THEMES_DELETE_TITLE'] = 'Supprimer thème';
+$langArr['MODAL-THEMES_DELETE_TEXT'] = 'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal theme add/edit
+$langArr['MODAL-THEMES_ADDEDIT_TITLE_ADD'] = 'Ajouter thème';
+$langArr['MODAL-THEMES_ADDEDIT_TITLE_EDIT'] = 'Modifier thème';
+
+$langArr['MODAL-THEMES_ADDEDIT_TXT_NAME'] = 'Nom du thème';
+$langArr['MODAL-THEMES_ADDEDIT_LBL_COLOURLIST'] = 'Liste de couleur:';
+$langArr['MODAL-THEMES_ADDEDIT_LBL_THEMECOLOURS'] = 'Couleurs des thèmes:';
+
+// Modal colour management
+$langArr['MODAL-COLOUR_MANAGE_TITLE'] = 'Gestion de couleur';
+$langArr['MODAL-COLOUR_MANAGE_LBL_COLOUR'] = 'Couleur';
+$langArr['MODAL-COLOUR_DELETE_TITLE'] = 'Supprimer couleur';
+$langArr['MODAL-COLOUR_DELETE_TEXT'] =  'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal colour add/edit
+$langArr['MODAL-COLOUR_ADDEDIT_TITLE_ADD'] = 'Ajouter couleur';
+$langArr['MODAL-COLOUR_ADDEDIT_TITLE_EDIT'] = 'Modifier couleur';
+$langArr['MODAL-COLOUR_ADDEDIT_TXT_NAME'] = 'Nom de couleur';
+$langArr['MODAL-COLOUR_ADDEDIT_TXT_COLOURVALUE'] = 'Valeur de couleur';
+$langArr['MODAL-COLOUR_ADDEDIT_TXT_BORDERCOLOURVALUE'] = 'Couleur de la bordure';
+$langArr['MODAL-COLOUR_ADDEDIT_SLDR_BORDERSIZE'] = 'Taille de la bordure (px)';
+
+// Modal groups management
+$langArr['MODAL-GROUPS_MANAGE_TITLE'] = 'Groupes';
+$langArr['MODAL-GROUPS_MANAGE_LBL_GROUP'] = 'Groupe';
+$langArr['MODAL-GROUPS_DELETE_TITLE'] = 'Supprimer groupe';
+$langArr['MODAL-GROUPS_DELETE_TEXT'] = 'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal groups member assignment
+$langArr['MODAL-GROUPS_MEMBERS_TITLE_ADMINS'] = 'Admins du groupe';
+$langArr['MODAL-GROUPS_MEMBERS_TITLE_MEMBERS'] = 'Usagers du groupe';
+$langArr['MODAL-GROUPS_MEMBERS_TXT_NAME'] = 'Nom du groupe';
+$langArr['MODAL-GROUPS_MEMBERS_LBL_USERS'] = 'Usagers';
+$langArr['MODAL-GROUPS_MEMBERS_LBL_SELECTEDUSERS'] ='Usagers sélectionnés';
+
+// Modal group add/edit
+$langArr['MODAL-GROUPS_ADDEDIT_TITLE'] = 'Groupe';
+$langArr['MODAL-GROUPS_ADDEDIT_TXT_NAME'] = 'Nom';
+$langArr['MODAL-GROUPS_ADDEDIT_TXT_NOTES'] = 'Notes';
+$langArr['MODAL-GROUPS_ADDEDIT_SELECT_PARENT'] = 'Groupe parent';
+
+// Modal role management
+$langArr['MODAL-ROLES_MANAGE_TITLE'] = 'Rôles';
+$langArr['MODAL-ROLES_MANAGE_LBL_ROLE'] = 'Rôle';
+$langArr['MODAL-ROLES_DELETE_TITLE'] = 'Supprimer rôle';
+$langArr['MODAL-ROLES_DELETE_TEXT'] ='Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal role member assignment
+$langArr['MODAL-ROLES_MEMBERS_TITLE'] = 'Membres du rôle';
+$langArr['MODAL-ROLES_MEMBERS_TXT_NAME'] = 'Nom du rôle';
+$langArr['MODAL-ROLES_MEMBERS_LBL_USERS'] = 'Usagers';
+$langArr['MODAL-ROLES_MEMBERS_LBL_SELECTEDUSERS'] = 'Usagers sélectionnés';
+
+// Modal role add/edit
+$langArr['MODAL-ROLE_ADDEDIT_TITLE_ADD'] = 'Ajouter rôle';
+$langArr['MODAL-ROLE_ADDEDIT_TITLE_EDIT'] = 'Modifier le rôle';
+$langArr['MODAL-ROLE_ADDEDIT_TXT_NAME'] = 'Nom de rôle';
+$langArr['MODAL-ROLE_ADDEDIT_TXT_NOTES'] = 'Notes';
+$langArr['MODAL-ROLE_ADDEDIT_LBL_PERMISSIONSOFF'] = 'Non autorisé';
+$langArr['MODAL-ROLE_ADDEDIT_LBL_PERMISSIONSON'] = 'Autorisé';
+
+// Modal data connection management
+$langArr['MODAL-DATACONNECTIONS_MANAGE_TITLE'] = 'Connexions de données';
+$langArr['MODAL-DATACONNECTIONS_MANAGE_LBL_DATACONNECTION'] = 'Connexion de données';
+$langArr['MODAL-DATACONNECTIONS_DELETE_TITLE'] = 'Supprimer connexion de données';
+$langArr['MODAL-DATACONNECTIONS_DELETE_TEXT'] = 'Êtes-vous certain que vous voulez supprimer \'{0}\'?';
+
+// Modal data connection member assignment
+$langArr['MODAL-DATACONNECTIONS_MEMBERS_TITLE'] = 'Membres connexion de données';
+$langArr['MODAL-DATACONNECTIONS_MEMBERS_TXT_NAME'] = 'Nom connexion de données';
+$langArr['MODAL-DATACONNECTIONS_MEMBERS_LBL_USERS'] = 'Usagers';
+$langArr['MODAL-DATACONNECTIONS_MEMBERS_LBL_SELECTEDUSERS'] = 'Usagers sélectionnés';
+
+// Modal data connection add/edit
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TITLE'] = 'Connexion de données';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_NAME'] = 'Nom';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_SERVER'] = 'Serveur';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_PORT'] = 'Port';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_INSTANCE'] = 'Instance de BD';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_DATABASE'] = 'Base de données';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_USERNAME'] = 'Nom d’utilisateur';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_TXT_PASSWORD'] = 'Mot de passe';
+$langArr['MODAL-DATACONNECTIONS_ADDEDIT_SELECT_SERVERTYPE'] = 'Type de serveur';
+
+// Modal loginas
+$langArr['MODAL-LOGINAS_TITLE'] ='Se connecter en tant que...';
+$langArr['MODAL-LOGINAS_SELECT_SELECTUSER'] = 'Sélectionner utilisateur';
+$langArr['MODAL-LOGINAS_BTN_LOGIN'] = 'Login';
+
+// Modal preferences
+$langArr['MODAL-PREFERENCES_TITLE'] = 'Préférences';
+$langArr['MODAL-PREFERENCES_SELECT_DEFAULTUSERROLE'] = 'Rôle d’utilisateur par défaut';
+$langArr['MODAL-PREFERENCES_SELECT_DEFAULTADMINROLE'] = 'Rôle d’admin par défaut';
+$langArr['MODAL-PREFERENCES_SELECT_DEFAULTDASHBOARD'] = 'Tableau de bord par défaut';
+
+// Modal logout
+$langArr['MODAL-SIGNOUT_TITLE'] = 'Se déconnecter';
+$langArr['MODAL-SIGNOUT_TXT_SIGNOUT'] = 'Êtes-vous certain que vous voulez vous déconnecter?';
+$langArr['MODAL-SIGNOUT_BTN_SIGNOUT'] = 'Se déconnecter';
+
+// Context menu
+$langArr['CONTEXT_DEFAULTDASHBOARD'] = 'Définir tableau de bord par défaut';
+$langArr['CONTEXT_EDITDASHBOARD'] = 'Modifier tableau de bord';
+$langArr['CONTEXT_RESETDASHBOARD'] = 'Réinitialiser tableau de bord';
+
+// Element Context menu
+$langArr['CONTEXT_ELEMENT_EDIT'] = 'Modifier';
+$langArr['CONTEXT_ELEMENT_APPEARANCE'] = 'Apparence';
+$langArr['CONTEXT_ELEMENT_CHART'] = 'Graphique';
+$langArr['CONTEXT_ELEMENT_HTML'] = 'Html';
+$langArr['CONTEXT_ELEMENT_ORDER'] = 'Ordre';
+$langArr['CONTEXT_ELEMENT_FORWARD'] = 'Avancer';
+$langArr['CONTEXT_ELEMENT_BACKWARD'] = 'Reculer';
+
+// Modal element appearance
+$langArr['MODAL-ELEMENT_APPEARANCE_TITLE'] = 'Apparence';
+$langArr['MODAL-ELEMENT_APPEARANCE_CHK_TITLE'] = 'Montrer le titre';
+$langArr['MODAL-ELEMENT_APPEARANCE_SELECT_TITLECOLOUR'] = 'Couleur du titre';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_HEIGHT'] = 'Hauteur';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_WIDTH'] = 'Largeur';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_TOP'] = 'Haut';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_LEFT'] = 'Gauche';
+$langArr['MODAL-ELEMENT_APPEARANCE_SELECT_BACKGROUNDCOLOR'] = 'Couleur de fond';
+$langArr['MODAL-ELEMENT_APPEARANCE_CHK_BORDER'] = 'Bordure';
+$langArr['MODAL-ELEMENT_APPEARANCE_SELECT_BORDERCOLOR'] = 'Couleur de la bordure';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_BORDERWIDTH'] = 'Largeur de la bordure';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_CORNERS'] = 'Angles (arc)';
+$langArr['MODAL-ELEMENT_APPEARANCE_TXT_ZINDEX'] = 'Z-Index';
+$langArr['MODAL-ELEMENT_APPEARANCE_LBL_ENABLE'] = 'Activer';
+
+// Footer
+$langArr['FOOTER_LINK'] = 'À propos';
+
+// Modal About
+$langArr['MODAL-ABOUT_TITLE'] = 'À propos';
+$langArr['MODAL-ABOUT_TXT_VERSION'] = 'Version';
+$langArr['MODAL-ABOUT_TXT_REV'] = 'Rev';
+$langArr['MODAL-ABOUT_TXT_COPYRIGHT'] = 'Copyright';
+$langArr['MODAL-ABOUT_TXT_INFO'] = 'Info';
+
+// Modal login
+$langArr['MODAL-LOGIN_TITLE'] = 'Login';
+$langArr['MODAL-LOGIN_TXT_LOGIN'] = 'Login';
+$langArr['MODAL-LOGIN_TXT_PASSWORD'] = 'Mot de passe';
+$langArr['MODAL-LOGIN_BTN_LOGIN'] = 'Login';
+
+// Tooltips
+$langArr['TOOLTIP_ADMINS'] = 'Admins';
+$langArr['TOOLTIP_USERS'] = 'Usagers';
+$langArr['TOOLTIP_SETTINGS'] = 'Paramètres';
+$langArr['TOOLTIP_DELETE'] = 'Supprimer';
+
+// Error strings
+$langArr['ERROR_USERINACCESSIBLE'] = 'Utilisateur inaccessible'; 
+$langArr['ERROR_PERMISSION'] = 'Permissions';
+$langArr['ERROR_GROUPINACCESSIBLE'] = 'Groupe inaccessible';
+$langArr['ERROR_DASHBOARDINACCESSIBLE'] = 'Tableau de bord inaccessible';
+$langArr['ERROR_DATACONNECTIONINACCESSIBLE'] = 'Connexion de données inaccessible';
+$langArr['ERROR_UNKNOWN'] = 'Inconnu';
+$langArr['ERROR_NOSESSION'] = 'Aucune session trouvée';
